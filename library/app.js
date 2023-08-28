92eb3d7065e8d7f9f436c36720fafd852c8a5388
salmanfarooqdev <slmnfrq94@gmail.com>
2023-08-28 20:11:04 +0500

    updated libary project to be in class structure

--- library/app.js
+++ library/app.js
@@ -1,49 +1,146 @@
-let overlay = document.querySelector('.overlay');
-let closebtn = document.querySelector('.close');
-let addbtn = document.querySelector('.addbook');
-let modal = document.querySelector('.modal-container');
-let bookgrid = document.querySelector('.bookgrid');
-let addbookform = document.querySelector('.addbookform');
-let deletebtn = document.querySelector('.delete');
 
+class Library{
+    constructor()
+    {
+        this.myLibrary = [];
+        this.overlay = document.querySelector('.overlay');
+        this.closebtn = document.querySelector('.close');
+        this.addbtn = document.querySelector('.addbook');
+        this.modal = document.querySelector('.modal-container');
+        this.bookgrid = document.querySelector('.bookgrid');
+        this.addbookform = document.querySelector('.addbookform');
+        this.deletebtn = document.querySelector('.delete');
 
-
-addbtn.addEventListener('click', function()
-{
-    overlay.classList.add('active');
-    modal.classList.add('active');
-});
-
-closebtn.addEventListener('click', function()
-{
-    overlay.classList.remove('active');
-    modal.classList.remove('active');
-});
-
-
-let myLibrary = [];
-
-function saveLibraryToLocalStorage() 
-{
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
-
-function loadLibraryFromLocalStorage() {
-    const savedLibrary = localStorage.getItem('myLibrary');
-    if (savedLibrary) {
-        myLibrary = JSON.parse(savedLibrary);
-    } else {
-        myLibrary = [];
+        this.loadLibraryFromLocalStorage();
+        this.setupEventListeners();
+        this.displayBooks();
     }
+
+    setupEventListeners() 
+    {
+        this.addbtn.addEventListener('click', () =>
+        {
+            this.overlay.classList.add('active');
+            this.modal.classList.add('active');
+        });
+        
+        this.closebtn.addEventListener('click', () =>
+        {
+            this.overlay.classList.remove('active');
+            this.modal.classList.remove('active');
+        });
+
+        this.addbookform.addEventListener('submit', () =>
+        {
+            event.preventDefault();
+            const name = document.querySelector('.inputTitle').value;
+            const author = document.querySelector('.inputAuthor').value;
+            const pages = document.querySelector('.inputPages').value;
+            const isread = document.querySelector('.checks').checked;
+
+
+            let book =  new Book(name, author, pages, isread);
+            this.addBookToLibrary(book);
+
+            this.overlay.classList.remove('active');
+            this.modal.classList.remove('active');
+
+            this.addbookform.reset();
+
+            this.displayBooks();
+        } );
+
+        document.addEventListener('click', (event) =>
+        {
+            if(event.target.classList.contains('delete'))
+            {
+                let bookIndex = event.target.dataset.index;
+                this.myLibrary.splice(bookIndex, 1);
+                this.saveLibraryToLocalStorage();
+                this.displayBooks();
+                
+            }
+        });
+
+        document.addEventListener('click', (event) =>
+        {
+            if(event.target.classList.contains('read'))
+            {
+                let bookIndex = event.target.dataset.index;
+               this.myLibrary[bookIndex].read = false;
+                this.saveLibraryToLocalStorage();
+                event.target.classList.remove('read');
+                event.target.classList.add('notread');
+                event.target.innerHTML='Not Read';
+
+
+            }
+            else if(event.target.classList.contains('notread'))
+            {
+                let bookIndex = event.target.dataset.index;
+                this.myLibrary[bookIndex].read = true;
+                this.saveLibraryToLocalStorage();
+                event.target.classList.remove('notread');
+                event.target.classList.add('read');
+                event.target.innerHTML='Read';
+            }
+        });
+    }
+
+    saveLibraryToLocalStorage() 
+    {
+        localStorage.setItem('myLibrary', JSON.stringify(this.myLibrary));
+    }
+
+    loadLibraryFromLocalStorage() {
+        const savedLibrary = localStorage.getItem('myLibrary');
+        if (savedLibrary) {
+            this.myLibrary = JSON.parse(savedLibrary);
+        } else {
+            this.myLibrary = [];
+        }
+    }
+
+    addBookToLibrary(nbook){
+        this.myLibrary.push(nbook);
+        this.saveLibraryToLocalStorage();
+    }
+
+    displayBooks()
+    {
+        let bookHTML = '';
+        this.myLibrary.forEach(function(book, index)
+        {
+            if(book.read === true)
+            {
+                bookHTML = bookHTML + ` <div class="box">
+                <div class="bookname">${book.name}</div>
+                <div class="bookauthor">${book.author}</div>
+                <div class="bookpages">${book.pages}</div>
+                <button class="read" data-index = "${index}">Read</button>
+                <button class="delete" data-index = "${index}">Delete</button>
+                </div>`;
+            }
+            else if(book.read=== false)
+            {
+                bookHTML = bookHTML + ` <div class="box">
+                <div class="bookname">${book.name}</div>
+                <div class="bookauthor">${book.author}</div>
+                <div class="bookpages">${book.pages}</div>
+                <button class="notread" data-index = "${index}">Not Read</button>
+                <button class="delete" data-index = "${index}">Delete</button>
+                </div>`;
+            }
+        
+        });
+
+        this.bookgrid.innerHTML = bookHTML;
+
+    }
+
+
 }
 
-loadLibraryFromLocalStorage();
-
-
-let name = "";
-let author = "";
-let pages = 0;
-let read = false;
 
 function Book(name, author, pages, read)
 {
@@ -53,110 +150,7 @@ function Book(name, author, pages, read)
     this.read = read;
 }
 
-function addBookToLibrary(nbook){
-
-   
-        myLibrary.push(nbook);
-        saveLibraryToLocalStorage();
-    
-}
-
-
-function displayBooks()
-{
-    let bookHTML = '';
-    myLibrary.forEach(function(book, index)
-    {
-        if(book.read === true)
-        {
-            bookHTML = bookHTML + ` <div class="box">
-            <div class="bookname">${book.name}</div>
-            <div class="bookauthor">${book.author}</div>
-            <div class="bookpages">${book.pages}</div>
-            <button class="read" data-index = "${index}">Read</button>
-            <button class="delete" data-index = "${index}">Delete</button>
-             </div>`;
-        }
-        else if(book.read=== false)
-        {
-            bookHTML = bookHTML + ` <div class="box">
-            <div class="bookname">${book.name}</div>
-            <div class="bookauthor">${book.author}</div>
-            <div class="bookpages">${book.pages}</div>
-            <button class="notread" data-index = "${index}">Not Read</button>
-            <button class="delete" data-index = "${index}">Delete</button>
-             </div>`;
-        }
-       
-    });
-
-    bookgrid.innerHTML = bookHTML;
-
-}
-
-
-
-addbookform.addEventListener('submit', function()
-{
-    event.preventDefault();
-    const name = document.querySelector('.inputTitle').value;
-    const author = document.querySelector('.inputAuthor').value;
-    const pages = document.querySelector('.inputPages').value;
-    const isread = document.querySelector('.checks').checked;
-
-
-    let book =  new Book(name, author, pages, isread);
-    addBookToLibrary(book);
-
-    overlay.classList.remove('active');
-    modal.classList.remove('active');
-
-    addbookform.reset();
-
-    displayBooks();
-
-   
-} );
-
-document.addEventListener('click', function(event)
-{
-    if(event.target.classList.contains('delete'))
-    {
-        let bookIndex = event.target.dataset.index;
-        myLibrary.splice(bookIndex, 1);
-        saveLibraryToLocalStorage();
-        displayBooks();
-        
-    }
-});
-
-document.addEventListener('click', function(event)
-{
-    if(event.target.classList.contains('read'))
-    {
-        let bookIndex = event.target.dataset.index;
-        myLibrary[bookIndex].read = false;
-        saveLibraryToLocalStorage();
-        event.target.classList.remove('read');
-        event.target.classList.add('notread');
-        event.target.innerHTML='Not Read';
-
-
-    }
-    else if(event.target.classList.contains('notread'))
-    {
-        let bookIndex = event.target.dataset.index;
-        myLibrary[bookIndex].read = true;
-        saveLibraryToLocalStorage();
-        event.target.classList.remove('notread');
-        event.target.classList.add('read');
-        event.target.innerHTML='Read';
-    }
-});
-
 window.onload = () => {
-    loadLibraryFromLocalStorage();
-
-    displayBooks();
+    const library = new Library();
    
 };
