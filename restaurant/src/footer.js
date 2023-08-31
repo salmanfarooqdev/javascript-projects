4c91df49539146108b2a36538b87a066088c0766
salmanfarooqdev <138154750+salmanfarooqdev@users.noreply.github.com>
2023-08-31 18:21:54 +0500

    Delete restaurant directory

--- restaurant/src/footer.js
+++
@@ -1,7 +0,0 @@
-export function generatefooter(){
-    const content = document.querySelector('.content');
-    let footer = document.createElement('div');
-    footer.classList.add('footer');
-    footer.innerHTML = "footers";
-    content.appendChild(footer);
-}
